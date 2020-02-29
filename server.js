cbf1d97873514889245e2676331aaf2281e7986e
Cory Marshman <marshman.cory@gmail.com>
2020-02-29 15:40:14 -0500

    api bug fixes

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const express = require("express");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-// routes here
+ mongoose.connect(MONGODB_URI);
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
